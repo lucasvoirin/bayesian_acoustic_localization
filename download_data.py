b4021804bce5882a5e8e9dfaef15aa86f51e2f64
lucasvoirin <lucas.voirin@mailo.com>
2026-03-27 20:06:01 -0400

    first test dowload data

---
+++ download_data.py
@@ -0,0 +1,47 @@
+from pyDataverse.api import NativeApi
+import requests
+import os
+from tqdm import tqdm
+
+BASE_URL = "https://borealisdata.ca"
+DOI = "doi:10.5683/SP3/AUWB3O"
+DIRS = ["test_localisation_20250808/SYNC"]
+FILES = ["test_localisation_20250808/master_propagation_2025_annotations.rds"]
+TAB_FILES = ["test_localisation_20250808/data.tab"]
+
+api = NativeApi(BASE_URL, API_TOKEN)
+dataset = api.get_dataset(DOI)
+files = dataset.json()["data"]["latestVersion"]["files"]
+
+# Filtrer les fichiers
+fichiers_a_telecharger = []
+for f in files:
+    directory = f.get("directoryLabel", "")
+    filename = f["dataFile"]["filename"]
+    filepath_relatif = os.path.join(directory, filename)
+
+    dans_dossier_cible = any(directory.startswith(d) for d in DIRS)
+    est_fichier_specifique = filepath_relatif in FILES
+    est_fichier_csv = filepath_relatif in TAB_FILES
+
+    if dans_dossier_cible or est_fichier_specifique or est_fichier_csv:
+        fichiers_a_telecharger.append((f, est_fichier_csv))
+
+# Télécharger avec barre de progression
+for f, est_fichier_csv in tqdm(fichiers_a_telecharger, desc="Téléchargement", unit="fichier"):
+    directory = f.get("directoryLabel", "")
+    filename = f["dataFile"]["filename"]
+    file_id = f["dataFile"]["id"]
+
+    if est_fichier_csv:
+        filename = filename.replace(".tab", ".csv")
+        url = f"{BASE_URL}/api/access/datafile/{file_id}?format=csv"
+    else:
+        url = f"{BASE_URL}/api/access/datafile/{file_id}"
+
+    filepath = os.path.join("dataset", directory, filename)
+    os.makedirs(os.path.dirname(filepath), exist_ok=True)
+
+    r = requests.get(url, headers={"X-Dataverse-key": API_TOKEN})
+    with open(filepath, "wb") as out:
+        out.write(r.content)
